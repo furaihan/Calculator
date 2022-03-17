abdfb26da4f40419a48c03a251d5a10aff206022
furaihan <28698437+furaihan@users.noreply.github.com>
2022-03-17 14:45:30 +0700

    menambahkan error handling loop

--- Kalkulator.cs
+++ Kalkulator.cs
@@ -3,39 +3,45 @@ using System;
 
 namespace PertemuanSatu
 {
-    private static class Program
+    internal static class Program
     {
         private static void Main(string[] args)
         {
             Console.Title = "Apikasi Kalkulator";
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Green;          
+            int a, b, pilihan;
 
-            Console.WriteLine("Pilih menu calculator");
-            Console.WriteLine();
-            Console.WriteLine("1. Penambahan");
-            Console.WriteLine("2. Pengurangan");
-            Console.WriteLine("3. Perkalian");
-            Console.WriteLine("4. Pembagian");
+            while (true)
+            {
+                Console.WriteLine("Pilih menu calculator");
+                Console.WriteLine();
+                Console.WriteLine("1. Penambahan");
+                Console.WriteLine("2. Pengurangan");
+                Console.WriteLine("3. Perkalian");
+                Console.WriteLine("4. Pembagian");
+                Console.WriteLine();
+                Console.Write("Input Nomor Menu [1..4]: ");
+                if (!int.TryParse(Console.ReadLine(), out int pilihantemp) || !(pilihantemp <= 4 && pilihantemp >= 1))
+                {
+                    DisplayError();
+                    continue;
+                }
+                Console.Write("Inputkan Nilai A: ");
+                if (!int.TryParse(Console.ReadLine(), out int atemp))
+                {
+                    DisplayError();
+                    continue;
+                }
+                Console.Write("Inputkan Nilai B: ");
+                if (!int.TryParse(Console.ReadLine(), out int btemp))
+                {
+                    DisplayError();
+                    continue;
+                }
+                a = atemp; b = btemp; pilihan = pilihantemp;
+                break;
 
-
-            Console.Write("Input Nomor Menu: ");
-            if (!int.TryParse(Console.ReadLine(), out int pilihan) || !(pilihan <=4 && pilihan >=1))
-            {
-                Console.WriteLine("Error, anda tidak memasukkan angka yang valid, ulangi");
-                return;
-            }
-            Console.Write("Inputkan Nilai A: ");
-            if (!int.TryParse(Console.ReadLine(), out int a))
-            {
-                Console.WriteLine("Error, anda tidak memasukkan angka yang valid, ulangi");
-                return;
-            }
-            Console.Write("Inputkan Nilai B: ");
-            if (!int.TryParse(Console.ReadLine(), out int b))
-            {
-                Console.WriteLine("Error, anda tidak memasukkan angka yang valid, ulangi");
-                return;
-            }
+            }          
             float hasil = pilihan switch
             {
                 1 => a + b,
@@ -56,6 +62,13 @@ namespace PertemuanSatu
             Console.WriteLine($"Hasil dari {a} {operasi} {b} = {hasil}");
             Console.WriteLine("\n Tekan sembarang tombol untuk keluar");
             Console.ReadKey();
+        }
+        private static void DisplayError()
+        {
+            Console.Clear();
+            Console.WriteLine("Error, anda tidak memasukkan angka yang valid, tekan sembarang tombol untuk mengulangi");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
